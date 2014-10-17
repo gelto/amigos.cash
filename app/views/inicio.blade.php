b0b9a64b33a4fd87e58bf6b00b6d086e04c3863e
Gelto <gelto@MacBook-Air-de-Gelto.local>
2014-10-16 22:40:42 -0500

    un push de hola mundo casi casi

---
+++ app/views/inicio.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.iniciolayout')
+@section('content')
+
+Hola amigos. Hice este sistema para que podamos llevar el control de los que nos prestamos día a día en la oficina. 
+<br><br>
+Aún estoy trabajando en el y con los días iré agregando funcionalidad.
+<br><br>
+Por favor sientanse en libertad de usarlo con su propia red de amigos con quienes tienen la confianza de prestarse dinero
+<br>
+para cosas inmediatas como el pago de la cuenta o el pasaje hasta cosas que requieren montos mayores como prestamos para 
+<br>
+la renta o algo así.
+<br><br>
+(:
+    	
+@stop
---
+++ app/views/layouts/iniciolayout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
+<head>
+     <title>Amigos Cash</title>
+     <meta charset="utf-8">
+     <meta name = "format-detection" content = "telephone=no" />
+     <link rel="icon" href="">
+     <link rel="shortcut icon" href="" />
+  <body>
+    @yield('content')
+  </body>
+</html>
